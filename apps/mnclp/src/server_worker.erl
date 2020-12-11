413491d8174a6dae6e661a5e08eed67e92bfc9a8
Sergey Zaikin <sergey.zaikin@creative-assembly.com>
2020-12-11 01:57:14 +0000

    4k data

--- apps/mnclp/src/server_worker.erl
+++ apps/mnclp/src/server_worker.erl
@@ -21,16 +21,35 @@ handle_cast(accept, S = #{socket := ListenSocket}) ->
   {ok, AcceptSocket} = gen_tcp:accept(ListenSocket),
   server_sup:start_socket(),
   send(AcceptSocket, "Send some data", []),
-  {noreply, S#{socket => AcceptSocket}};
+  {noreply, S#{socket => AcceptSocket, partial_data => undef}};
 handle_cast(E, S) ->
   io:format("unexpected: ~p~n", [E]),
   {noreply, S}.
 
-handle_info({tcp, _Socket, Str}, S = #{socket := Socket}) ->
-  Encoded = kv_pb:decode_msg(Str, req_envelope),
-  Resp = process_request(Encoded),
-  send(Socket, Resp),
-  {noreply, S};
+handle_info({tcp, _Socket, Str}, S = #{socket := Socket, partial_data := PartialData0}) ->
+  RecordOrError = case PartialData0 of
+                    undef -> try
+                               kv_pb:decode_msg(Str, req_envelope)
+                             catch _ -> error
+                             end;
+                    _ -> try
+                           kv_pb:decode_msg(<<PartialData0/binary, Str/binary>>, req_envelope)
+                         catch _ -> error
+                         end
+                  end,
+  case RecordOrError of
+    error ->
+      PartialData = case PartialData0 of
+                      undef -> Str;
+                      _ -> <<PartialData0/binary, Str/binary>>
+                    end,
+      inet:setopts(Socket, [{active, once}]),
+      {noreply, S#{partial_data => PartialData}};
+    Encoded ->
+      Resp = process_request(Encoded),
+      send(Socket, Resp),
+      {noreply, S#{partial_data => undef}}
+  end;
 handle_info({tcp_closed, _Socket}, S) ->
   {stop, normal, S};
 handle_info({tcp_error, _Socket, _}, S) ->
@@ -63,13 +82,13 @@ process_request(#req_envelope{type = set_request_t} = Req) ->
 
   DdbPid = mnclp_sup:get_ddb_worker_pid(),
   Resp = case gen_server:call(DdbPid, {put_data, [{<<"key">>, {b, Key}},
-                                                  {<<"value">>, Value},
-                                                  {<<"vsn">>, <<"1">>}]}) of
+    {<<"value">>, Value},
+    {<<"vsn">>, <<"1">>}]}) of
            {ok, _} -> ok;
            {error, _} -> internal
          end,
   kv_pb:encode_msg(#req_envelope{type = set_response_t,
-                                 set_resp = #set_response{error = Resp}});
+    set_resp = #set_response{error = Resp}});
 process_request(#req_envelope{type = get_request_t} = Req) ->
   Key = Req#req_envelope.get_req#get_request.key,
 
